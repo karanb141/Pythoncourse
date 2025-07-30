a502282489c72b74affe2cef5903618685740bb2
karanb141 <63932525+karanb141@users.noreply.github.com>
2025-07-30 17:10:03 -0400

    Create custom_module.py

---
+++ custom_module.py
@@ -0,0 +1,3 @@
+def generate_time_travel_message(year, destination, cost):
+  answer = "pack your bags! you're going to {destination} in the year {year} and it will only cost {cost}".format(destination=destination, year=year, cost=cost)
+  return answer
